a693a69728ca73fda4c4ab8c295c7da8a2594485
Toby Clemson <tobyclemson@gmail.com>
2017-09-23 13:39:01 +0100

    Add static factory method on Confidante.

--- lib/confidante.rb
+++ lib/confidante.rb
@@ -5,4 +5,7 @@ require 'hiera/backend/env_backend'
 require 'hiera/backend/overrides_backend'
 
 module Confidante
+  def self.configuration(opts = {})
+    Confidante::Configuration.new(opts)
+  end
 end
