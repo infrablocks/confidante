63203eaae07cc8872bcb07c31b20e5f40c2cc695
Toby Clemson <tobyclemson@gmail.com>
2021-04-03 12:40:34 +0100

    Add coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
+SimpleCov.start do
+  enable_coverage :branch
+  minimum_coverage line: 97.7, branch: 100
+end
+
 require 'bundler/setup'
 require 'confidante'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
+  config.filter_run_when_matching :focus
   config.example_status_persistence_file_path = '.rspec_status'
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
